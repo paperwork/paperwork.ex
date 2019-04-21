c10e61cb40fba2976fc11af7a29c31009f4e896c
Marius <marius@twostairs.com>
2019-04-22 00:25:24 +0200

    Added dummy config

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,15 @@
 use Mix.Config
 
+config :paperwork, :server,
+    app: :paperwork,
+    cache_ttl_default: 86_400,
+    cache_janitor_interval: 60
+
+config :paperwork, :internal,
+    cache_ttl: 60,
+    configs: {:system, :string, "INTERNAL_RESOURCE_CONFIGS", "http://localhost:8880/internal/configs"},
+    users:   {:system, :string, "INTERNAL_RESOURCE_USERS",   "http://localhost:8881/internal/users"},
+    notes:   {:system, :string, "INTERNAL_RESOURCE_NOTES",   "http://localhost:8882/internal/notes"}
+
 config :logger,
     backends: [:console]
