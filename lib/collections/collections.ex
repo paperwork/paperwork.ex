0bcea5f982dc5a58934b2e34dd2514ab8770e63e
Marius <marius@twostairs.com>
2019-04-22 00:23:37 +0200

    Fruther enhancements to Collections

--- lib/collections/collections.ex
+++ lib/collections/collections.ex
@@ -22,19 +22,9 @@ defmodule Paperwork.Collections do
                 end
             end
 
-            def keys_to_atoms(string_key_map) when is_map(string_key_map) do
-                for {key, val} <- string_key_map, into: %{} do
-                    case Enumerable.impl_for val do
-                        nil -> {String.to_atom(key), val}
-                        _ -> {String.to_atom(key), keys_to_atoms(val)}
-                    end
-                end
-            end
-            def keys_to_atoms(value), do: value
-
             @spec found_or_nil(result :: Map.t) :: {:ok, %__MODULE__{}}
             defp found_or_nil(%{"_id" => _id} = result) do
-                {:ok, (struct(__MODULE__, keys_to_atoms(result)) |> Map.put(:id, result["_id"]))}
+                {:ok, (struct(__MODULE__, Paperwork.Collections.keys_to_atoms(result)) |> Map.put(:id, result["_id"]))}
             end
 
             @spec found_or_nil(result :: {:ok, Map.t}) :: {:ok, %__MODULE__{}}
@@ -52,6 +42,16 @@ defmodule Paperwork.Collections do
 
             @spec found_or_nil(result :: nil) :: {:notfound, nil}
             defp found_or_nil(nil = result) do
+                {:notfound, nil}
+            end
+
+            @spec found_or_nil(result :: {:ok, nil}) :: {:notfound, nil}
+            defp found_or_nil({:ok, nil}) do
+                {:notfound, nil}
+            end
+
+            @spec found_or_nil(result :: []) :: {:notfound, nil}
+            defp found_or_nil([] = result) when is_list(result) do
                 {:notfound, nil}
             end
 
@@ -94,42 +94,78 @@ defmodule Paperwork.Collections do
                 end
             end
 
+            defp real_value(value, key) do
+                case key do
+                    :id -> real_value_for_id(value)
+                    other -> value
+                end
+            end
+
+            defp real_value_for_id(%BSON.ObjectId{} = value) when is_map(value) do
+                value
+            end
+
+            defp real_value_for_id(value) when is_binary(value) do
+                BSON.ObjectId.decode!(value)
+            end
+
             @spec collection_find(query :: Map.t, expect_many :: Boolean.t) :: {:ok, %__MODULE__{}} | {:ok, [%__MODULE__{}]} | {:notfound, nil}
-            def collection_find(%{} = query, expect_many) when is_boolean(expect_many) and expect_many == true do
+            def collection_find(%{} = query, expect_many) when is_map(query) and is_boolean(expect_many) and expect_many == true do
                 Mongo.find(:mongo, @collection, query, pool: DBConnection.Poolboy)
                 |> Enum.to_list
                 |> found_or_nil
             end
 
             @spec collection_find(query :: Map.t, expect_many :: Boolean.t) :: {:ok, %__MODULE__{}} | {:ok, [%__MODULE__{}]} | {:notfound, nil}
-            def collection_find(%{} = query, expect_many) when is_boolean(expect_many) and expect_many == false do
+            def collection_find(%{} = query, expect_many) when is_map(query) and is_boolean(expect_many) and expect_many == false do
                 Mongo.find_one(:mongo, @collection, query, pool: DBConnection.Poolboy)
                 |> found_or_nil
             end
 
-            @spec collection_find(query :: %__MODULE__{}, by_key :: Atom.t, expect_many :: Boolean.t) :: {:ok, %__MODULE__{}} | {:ok, [%__MODULE__{}]} | {:notfound, nil}
-            def collection_find(%__MODULE__{} = model, by_key, expect_many \\ false) do
-                collection_find(%{real_key(by_key) => Map.get(model, by_key)}, expect_many)
+            @spec collection_find(model :: %__MODULE__{}, by_key :: Atom.t, expect_many :: Boolean.t) :: {:ok, %__MODULE__{}} | {:ok, [%__MODULE__{}]} | {:notfound, nil}
+            def collection_find(%__MODULE__{} = model, by_key, expect_many \\ false) when is_map(model) and is_atom(by_key) and is_boolean(expect_many) do
+                collection_find(%{real_key(by_key) => Map.get(model, by_key) |> real_value(by_key)}, expect_many)
             end
 
-            @spec collection_insert(query :: %__MODULE__{}) :: {:ok, %__MODULE__{}} | {:error, String.t}
+            @spec collection_insert(model :: %__MODULE__{}) :: {:ok, %__MODULE__{}} | {:error, String.t}
             def collection_insert(%__MODULE__{} = model) do
                 Mongo.insert_one(:mongo, @collection, Map.delete(model, :id), pool: DBConnection.Poolboy)
                 |> ok_or_error(:inserted_id, model)
             end
 
-            @spec collection_update(query :: %__MODULE__{}, filter_key :: Atom.t) :: {:ok, %__MODULE__{}} | {:error, String.t}
+            @spec collection_update(model :: %__MODULE__{}, filter_key :: Atom.t) :: {:ok, %__MODULE__{}} | {:error, String.t}
             def collection_update(%__MODULE__{} = model, filter_key) do
-                Mongo.find_one_and_update(:mongo, @collection, %{real_key(filter_key) => Map.get(model, filter_key)}, %{"$set": (Map.from_struct(model) |> Enum.filter(fn {k, v} -> v != nil && k != filter_key end) |> Enum.into(%{}))}, pool: DBConnection.Poolboy, return_document: :after)
+                Mongo.find_one_and_update(:mongo, @collection, %{real_key(filter_key) => Map.get(model, filter_key) |> real_value(filter_key)}, %{"$set": (Map.from_struct(model) |> Enum.filter(fn {k, v} -> v != nil && k != filter_key end) |> Enum.into(%{}))}, pool: DBConnection.Poolboy, return_document: :after)
                 |> found_or_nil
             end
 
-            @spec collection_update(query :: %__MODULE__{}) :: {:ok, %__MODULE__{}} | {:error, String.t}
+            @spec collection_update(model :: %__MODULE__{}) :: {:ok, %__MODULE__{}} | {:error, String.t}
             def collection_update(%__MODULE__{} = model) do
                 collection_update(model, :id)
             end
 
+            @spec collection_update_manually(set :: %{}, query :: %{}) :: {:ok, %__MODULE__{}} | {:error, String.t}
+            def collection_update_manually(%{} = set, %{} = query) do
+                real_query = query
+                             |> Map.keys()
+                             |> Enum.map(fn (query_key) -> %{real_key(query_key) => Map.get(query, query_key) |> real_value(query_key)} end)
+                             |> Enum.reduce(fn (query_map, previous_query_map) -> Map.merge(previous_query_map, query_map) end)
+
+                Mongo.find_one_and_update(:mongo, @collection, real_query, set, pool: DBConnection.Poolboy, return_document: :after)
+                |> found_or_nil
+            end
+
         end
     end
+
+    def keys_to_atoms(string_key_map) when is_map(string_key_map) do
+        for {key, val} <- string_key_map, into: %{} do
+            case Enumerable.impl_for val do
+                nil -> {String.to_atom(key), val}
+                _ -> {String.to_atom(key), keys_to_atoms(val)}
+            end
+        end
+    end
+    def keys_to_atoms(value), do: value
 end
 
