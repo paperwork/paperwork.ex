ba9ac2a724513e4cdc584f9ddda63ee2e39574bc
Marius <marius@twostairs.com>
2019-05-12 13:40:44 +0200

    Added ! functions, fixed parameters

--- lib/internal/request.ex
+++ lib/internal/request.ex
@@ -33,6 +33,10 @@ defmodule Paperwork.Internal.Request do
     end
 
     def user(user_id) when is_binary(user_id) do
+        user(user_id, true)
+    end
+
+    def user(user_id, true=_cached) when is_binary(user_id) do
         url = "#{Paperwork.Internal.Resource.users()}/#{user_id}"
         request_get_cached(url)
     end
@@ -42,7 +46,18 @@ defmodule Paperwork.Internal.Request do
         request_get(url)
     end
 
+    def user!(user_id, cached \\ true) when is_binary(user_id) do
+        case user(user_id, cached) do
+            {:ok, response} -> response
+            _ -> nil
+        end
+    end
+
     def config(config_id) when is_binary(config_id) do
+        config(config_id, true)
+    end
+
+    def config(config_id, true=_cached) when is_binary(config_id) do
         url = "#{Paperwork.Internal.Resource.configs()}/#{config_id}"
         request_get_cached(url)
     end
@@ -50,5 +65,12 @@ defmodule Paperwork.Internal.Request do
     def config(config_id, false=_cached) when is_binary(config_id) do
         url = "#{Paperwork.Internal.Resource.configs()}/#{config_id}"
         request_get(url)
+    end
+
+    def config!(config_id, cached \\ true) when is_binary(config_id) do
+        case config(config_id, cached) do
+            {:ok, response} -> response
+            _ -> nil
+        end
     end
 end
