4ae838f76bea448709ec8c766be8792739573e60
Marius <marius@twostairs.com>
2019-05-20 21:06:03 +0200

    Added storages resource

--- lib/internal/resource.ex
+++ lib/internal/resource.ex
@@ -1,5 +1,6 @@
 defmodule Paperwork.Internal.Resource do
-    def configs(), do: Confex.fetch_env!(:paperwork, :internal)[:configs]
-    def users(),   do: Confex.fetch_env!(:paperwork, :internal)[:users]
-    def notes(),   do: Confex.fetch_env!(:paperwork, :internal)[:notes]
+    def configs(),  do: Confex.fetch_env!(:paperwork, :internal)[:configs]
+    def users(),    do: Confex.fetch_env!(:paperwork, :internal)[:users]
+    def notes(),    do: Confex.fetch_env!(:paperwork, :internal)[:notes]
+    def storages(), do: Confex.fetch_env!(:paperwork, :internal)[:storages]
 end
