0fde54893ba1782240b2d7ad2dcc8202892dc58f
Marius <marius@twostairs.com>
2019-05-25 19:32:05 +0200

    Implemented validate_gid, refactored code, TODO

--- lib/id/id.ex
+++ lib/id/id.ex
@@ -12,19 +12,27 @@ defmodule Paperwork.Id do
         id: "",
         system_id: ""
 
+    def split_gid(gid) when is_binary(gid), do: String.split(gid, "@")
+
     def from_gid(gid) when is_binary(gid) do
         {:ok, config_system_id} = Paperwork.Internal.Request.config("system_id")
 
-        resource_gid = String.split(gid, "@")
+        resource_gid =
+            gid
+            |> split_gid()
+
         resource_id =
             resource_gid
             |> Enum.at(0)
+
+        # TODO: The initially provided system_id (if any) is being overwritten here.
+        # This might break stuff. Think of how to implement this correctly.
         resource_system_id =
             resource_gid
             |> Enum.at(1,
                 config_system_id
-                |> Map.get("value")
-            )
+                |> Map.get("value"))
+
 
         %__MODULE__{
             gid: gid,
@@ -32,5 +40,14 @@ defmodule Paperwork.Id do
             system_id: resource_system_id
         }
     end
+
+    def validate_gid(gid) when is_binary(gid) do
+        case Regex.match?(~r/([0-9a-f]{24}){1}@([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}){1}/, gid) do
+            true -> {:ok, gid}
+            false -> {:error, "Not a global ID"}
+        end
+    end
+
+    def validate_gid(gid) when is_binary(gid) == false, do: {:error, "Not a global ID"}
 end
 
